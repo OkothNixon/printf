416904e1d75809b99abdc68127c52da779ef95f6
Nixon Okoth <rgnixonroy@gmail.com>
2022-04-19 19:36:23 +0300

    task 0

--- 0-character_operations.c
+++ 0-character_operations.c
@@ -14,25 +14,3 @@ int print_c(va_list ar_list)
 
 	return (_putchar(c));
 }
-
-/**
-* print_s - print string (%s)
-* @ar_list: Store the a list of characters
-* @va_list: list characters
-* Return: num of parameters printed
-*/
-
-int print_s(va_list ar_list)
-{
-	int i, count = 0;
-	char *str;
-
-	str = va_arg(ar_list, char *);
-	if (str == NULL)
-		str = "(null)";
-
-	for (i = 0; str[i]; i++)
-		count += _putchar(str[i]);
-
-	return (count);
-}
